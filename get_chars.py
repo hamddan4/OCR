0ae51ef79069ba38316fdda21710c973d419017b
hamddan4 <hamddan4@gmail.com>
2017-05-27 16:40:19 +0200

    Spot deletion OK

--- get_chars.py
+++ get_chars.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Wed May 10 18:33:44 2017
 
@@ -7,22 +7,26 @@ Created on Wed May 10 18:33:44 2017
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import color, filters, io
-from skimage.morphology import label
+from skimage.morphology import label, remove_small_holes, remove_small_objects
 from skimage.measure import regionprops
+
+from utils import oddNum
 
 import cv2
 
 import scipy.ndimage as nd
 
 max_plots = 0
-def plt_i(im):
+def plt_i(im,title=None):
     global max_plots
     max_plots += 1
     if(max_plots < 20):
         plt.figure(), plt.imshow(im,cmap=plt.cm.gray)
     else:
-        raise Exception("Too many plots are open!")
+        raise Exception("Too many plots are open! I might just saved your ass here...")
 #        pass
+    if not(title == None):
+        plt.title(title)
 
 def plt_s(regions):
     for prop in regions:
@@ -36,33 +40,46 @@ def plt_s(regions):
     
 def resizing(im, params):
     contour = params["contour"]
-    x, y = im.shape
-    dismx = x / contour
-    dimx = x / dismx
-    dimy = y / dismx
-    im2 = cv2.resize(im, (dimy, dimx)) 
+    im2 = im
+    if(contour != None):
+        x, y = im.shape
+        dismx = x / float(contour)
+        dimx = int(x / dismx)
+        dimy = int(y / dismx)
+        im2 = cv2.resize(im, (dimy, dimx)) 
     return im2    
       
-def neutre(im):  
-    
+def neutre(im, params):  
     [x, y] = im.shape
-    kernel = np.ones((x/100,x/100),np.uint8)
-    resd = cv2.erode(cv2.dilate(np.int16(im*255),kernel,1),kernel,1)
-    plt_i(resd)
-    plt.title('closed for neutralization');
-    im = im/resd
-    
+    kernel = np.ones((x/85,x/85),np.uint8)
+    resd = cv2.erode(cv2.dilate(np.int16(im),kernel,1),kernel,1)
+    if(params["TEST_MODE"]["im_treatement"]): 
+        plt_i(resd)
+        plt.title('closed for neutralization');
+    resd = np.array(resd, dtype=np.float)
+    im = np.divide(im,resd)
     return im
+
+def get_h_w(region):
+    minr, minc, maxr, maxc = region['BoundingBox']
+    height = int(maxc-minc)
+    width = int(maxr-minr)
+    return height, width
     
-def get_means(regions):
-    hight = []
+def get_lists_regions(regions):
+    height = []
     width = []
     for prop in regions:      
         minr, minc, maxr, maxc = prop['BoundingBox']
-        hight.append(int(maxc-minc))
+        height.append(int(maxc-minc))
         width.append(int(maxr-minr))
     
-    m_height = np.median(hight);
+    return height, width
+def get_means(regions):
+    
+    height, width = get_lists_regions(regions)
+    
+    m_height = np.median(height);
     m_width = np.median(width)
     return m_height, m_width
 
@@ -78,56 +95,72 @@ def split_im_regions(im, regions, mean_height):
     
 def apply_threshold(im,params):
     x, y = im.shape
-    
-    ws = x / 10
-    ws = ws + np.uint(not(ws%2))
+    print im
+    ws = oddNum(x / 10)
     
     sauv = filters.threshold_sauvola(im, window_size = ws)
-    
+    sauv = np.nan_to_num(sauv)
     if(params["TEST_MODE"]["im_treatement"]): 
         plt_i(sauv) 
         plt.title('sauvola')
     
-    res = im>sauv
+    res = np.greater(im,sauv)
     #kernel = np.ones((6,1),np.uint8)
+    
     return res
 
 def noise_removal(im, params):
-    im = np.uint8(im*255)
     
     blur = cv2.GaussianBlur(im,(9,9),0)
     ret2,th2 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     
     imb = (im + th2)
-    if(params["TEST_MODE"]["im_treatement"]): 
-        plt_i(imb)
-        plt.title('Otsu thresholding + Gaussian filtering')
-
+    return imb
+    
+def del_big_spots(im, params):
     #Fem un close per detectar regions o taques massa grans
-    x, y = im.shape
-    sz = x / 250
-    kernel = np.ones((sz,sz),np.uint8)
-    imc = cv2.erode(imb,kernel,1)
+    im = np.array(im, dtype=np.uint8)
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(2,2))
+    imb = cv2.erode(im, kernel,iterations=1)
     
-    if(params["TEST_MODE"]["im_treatement"]): 
-        plt_i(imc) 
-        plt.title('noise closed')
-    
-    neg = (255-imc)/255
-    label_image = label(neg)
-    plt_i(label_image)
-    
+    label_image = label(1-imb)
     regions = regionprops(label_image)
-    plt_i(neg)
-    plt.hold(True)
-    plt_s(regions)
-    plt.hold(False)
     
-    height, width = get_means(regions)
+    areas = np.array([x.area for x in regions])
+#    m_area = np.mean(areas)
+    thr = np.mean(areas)*6 + np.std(areas)
     
+    im2 = np.uint8(remove_small_holes(imb,  thr))
+    res= np.array(np.array((1-im2 + im), dtype=bool), dtype=np.uint8)
     
-    return im
+    plt_i(im,"im")
+    plt_i(imb,"imb")
+    plt_i(im2,"imb2")
+    plt_i(res, "res")
+#    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(2,2))
+#    im3 = cv2.erode(im3,kernel,1)
+    return res
 
+def del_small_spots(im, params):
+    #Fem un close per detectar regions o taques massa grans
+
+    
+#    im = 1-im
+    im = np.array(im, dtype=np.uint8)
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(1,1))
+    imb = cv2.erode(im, kernel)
+    
+    label_image = label(1-imb)
+    regions = regionprops(label_image)
+    
+    areas = np.array([x.area for x in regions])
+    m_area = np.mean(areas)
+
+    im2 = np.uint8(remove_small_holes(imb,  m_area/6))
+#    im2 = np.uint8(remove_small_objects(imb,  moda_area * 3))
+
+    return im2
+    
 def close_vert_median(im):
     #Labeling the image in order to get mean of height of the regions
     label_image = label(im)
@@ -248,32 +281,53 @@ def get_all(params):
     word conaining in a form of list of images of letters.
     *BETA 1. BY THE MOMENT IT ONLY RETURNS THE IMAGES OF LETTERS*
     '''
-    im = color.rgb2gray(io.imread("./img/"+params["image_name"]))
+    im = io.imread("./img/"+params["image_name"]);
+
+    if(len(im.shape) != 2):
+        im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+
+    
     if(params["TEST_MODE"]["im_treatement"]): 
         plt_i(im)
         plt.title('original')
-    
-#    im = neutre(im)
-#    plt_i(im)
-#    plt.title('neutre');
     
     im = resizing(im, params)
     if(params["TEST_MODE"]["im_treatement"]): 
         plt_i(im)
         plt.title('rezised')
     
+    im = noise_removal(im, params)
+    if(params["TEST_MODE"]["im_treatement"]): 
+        plt_i(im)
+        plt.title('Otsu thresholding + Gaussian filtering \n (noise removed)')    
+
+    
+    im = neutre(im, params)
+    if(params["TEST_MODE"]["im_treatement"]): 
+        plt_i(im)
+        plt.title('neutre');    
+    
     im = apply_threshold(im, params)
     if(params["TEST_MODE"]["im_treatement"]): 
         plt_i(im)
         plt.title('thresholded')
 #    plt.close('all')    
-    im = noise_removal(im, params)
+    
+#    im = np.uint8(im)*255
+    
+    im = del_big_spots(im, params)
     if(params["TEST_MODE"]["im_treatement"]): 
         plt_i(im)
-        plt.title('noise removed')
+        plt.title('big spots deleted')
+            
+    im = del_small_spots(im, params)
+    if(params["TEST_MODE"]["im_treatement"]): 
+        plt_i(im)
+        plt.title('small spots deleted')
     
     
-#    imb, mean_height = close_vert_median(imb)
+        
+    imb, mean_height = close_vert_median(im)
     label_image = label(im)
     regions = regionprops(label_image)
     height, width = get_means(regions)
